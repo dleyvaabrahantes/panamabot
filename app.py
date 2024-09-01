cd6b58fd70e4ca34bebd038c4f7e7a90af85e40b
David Leyva <dleyvaabrahantes@yahoo.com>
2024-09-01 17:54:11 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,53 @@
+import requests
+import time
+import urllib3
+
+# Suprimir las advertencias de seguridad
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Configuración del bot de Telegram
+token = '7258309648:AAFfsxkBnk7FHkwmJZpK3QeDh6HjxdzG2r8'
+chat_id = '722567297'
+telegram_url = f'https://api.telegram.org/bot{token}/sendMessage'
+
+# URL que vamos a monitorear
+url_to_monitor = 'https://visas.migracion.gob.pa/SIVA/verif_citas/'
+
+# Variable para almacenar el contenido anterior
+previous_content = "Dummy"
+
+def send_telegram_message(message):
+    payload = {
+        'chat_id': chat_id,
+        'text': message
+    }
+    response = requests.post(telegram_url, data=payload)
+    if response.status_code == 200:
+        print('Mensaje enviado exitosamente!')
+    else:
+        print('Error al enviar el mensaje:', response.status_code)
+
+while True:
+    try:
+        # Realizar la solicitud a la URL sin verificar SSL
+        response = requests.get(url_to_monitor, verify=False)
+        if response.status_code == 200:
+            current_content = response.text
+            
+            # Comparar con el contenido anterior
+            if previous_content is not None and current_content != previous_content:
+                # Si hay un cambio, enviar la notificación por Telegram
+                for _ in range(5):  # Enviar el mensaje 5 veces
+                    send_telegram_message('¡Cambio detectado en la URL monitoreada!')
+                    time.sleep(2)  # Esperar 2 segundos entre mensajes
+
+            # Actualizar el contenido anterior
+            previous_content = current_content
+        else:
+            print(f'Error al acceder a la URL: {response.status_code}')
+        
+    except Exception as e:
+        print(f'Error durante la solicitud: {e}')
+    
+    # Esperar un minuto antes de la siguiente revisión
+    time.sleep(60)
